492d6918bf0dcefd289ef04e538fbd78eee8ce84
Automated <actions@users.noreply.github.com>
2021-11-01 19:27:04 -0400

    Trying to get yesterday's data in the cloud

--- gainersLosers.py
+++ gainersLosers.py
@@ -8,9 +8,9 @@ import time
 import requests
 from datawrapper import Datawrapper
 
-ACCESS_TOKEN = os.getenv('DW_API_KEY')
+# ACCESS_TOKEN = os.getenv('DW_API_KEY')
 
-dw = Datawrapper(access_token=ACCESS_TOKEN)
+# dw = Datawrapper(access_token=ACCESS_TOKEN)
 
 def updateChart(dw_chart_id, dataSet, updateDate, dw_api_key):
     dw.add_data(
@@ -59,7 +59,7 @@ spTickers = ' '.join(sAndP['Symbol'])
 
 glRaw = yf.download(
         tickers = spTickers,
-        period = '2d',
+        period = '1d',
         interval = '1d',
         group_by = 'ticker',
         auto_adjust = False,
@@ -72,72 +72,72 @@ gl = glRaw.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1)
 
 gl.reset_index(level=0,inplace=True)
 
-gl.to_csv('rawYFinance.csv', index=False)
+gl.to_csv('sAndPYesterday.csv', index=False)
 # gl = pd.read_csv('rawYFinance.csv')
 
-maxDate = gl['Date'].max()
-minDate = gl['Date'].min()
+# maxDate = gl['Date'].max()
+# minDate = gl['Date'].min()
 
-todayGL = gl[gl['Date'] == maxDate].sort_values(by='Ticker')
-yesterdayGL = gl[gl['Date'] == minDate].sort_values(by='Ticker') 
+# todayGL = gl[gl['Date'] == maxDate].sort_values(by='Ticker')
+# yesterdayGL = gl[gl['Date'] == minDate].sort_values(by='Ticker') 
 
-todayGL.rename(columns={'Date': 'Today Date', 
-                        'Adj Close': 'Today Adj Close', 
-                        'Close': 'Today Close', 
-                        'High': 'Today High',
-                        'Low': 'Today Low',
-                        'Open': 'Today Open',
-                        'Volume': 'Today Volume'}, inplace=True)
+# todayGL.rename(columns={'Date': 'Today Date', 
+#                         'Adj Close': 'Today Adj Close', 
+#                         'Close': 'Today Close', 
+#                         'High': 'Today High',
+#                         'Low': 'Today Low',
+#                         'Open': 'Today Open',
+#                         'Volume': 'Today Volume'}, inplace=True)
 
-yesterdayGL.rename(columns={'Date': 'Yesterday Date', 
-                        'Adj Close': 'Yesterday Adj Close', 
-                        'Close': 'Yesterday Close', 
-                        'High': 'Yesterday High',
-                        'Low': 'Yesterday Low',
-                        'Open': 'Yesterday Open',
-                        'Volume': 'Yesterday Volume'}, inplace=True)
+# yesterdayGL.rename(columns={'Date': 'Yesterday Date', 
+#                         'Adj Close': 'Yesterday Adj Close', 
+#                         'Close': 'Yesterday Close', 
+#                         'High': 'Yesterday High',
+#                         'Low': 'Yesterday Low',
+#                         'Open': 'Yesterday Open',
+#                         'Volume': 'Yesterday Volume'}, inplace=True)
 
-comboGL = pd.merge(todayGL, yesterdayGL, on='Ticker', how='inner')
+# comboGL = pd.merge(todayGL, yesterdayGL, on='Ticker', how='inner')
 
-def getPctChg(New, Old):
-    pctChg = (New - Old) / (Old)
-    return pctChg
+# def getPctChg(New, Old):
+#     pctChg = (New - Old) / (Old)
+#     return pctChg
 
-glPctChg = list(map(lambda x,y: getPctChg(x, y), comboGL['Today Close'],comboGL['Yesterday Close']))
+# glPctChg = list(map(lambda x,y: getPctChg(x, y), comboGL['Today Close'],comboGL['Yesterday Close']))
 
-comboGL.rename(columns={'Today Date': 'Date', 'Today Close': 'Close'}, inplace=True)
+# comboGL.rename(columns={'Today Date': 'Date', 'Today Close': 'Close'}, inplace=True)
 
-fullGL = comboGL[['Date', 'Ticker', 'Close']]
-fullGL['1 Day Returns'] = pd.Series(glPctChg).values
-fullGL['Close'] = [round(x, 2) for x in fullGL['Close']]
+# fullGL = comboGL[['Date', 'Ticker', 'Close']]
+# fullGL['1 Day Returns'] = pd.Series(glPctChg).values
+# fullGL['Close'] = [round(x, 2) for x in fullGL['Close']]
 
-fullGL = fullGL.merge(sAndPRef, how='left', on='Ticker')
-
-
-biggestLosers = fullGL.sort_values(by=['1 Day Returns']).iloc[0:8,:].reset_index()
-biggestGainers = fullGL.sort_values(by=['1 Day Returns'], ascending=False).iloc[0:8,:].reset_index()
-
-biggestLosers.rename(columns={'1 Day Returns':'% Loss'}, inplace=True)
-biggestGainers.rename(columns={'1 Day Returns':'% Gain'}, inplace=True)
-
-biggestLosers.rename(columns={'Close':'Price\u061C'}, inplace=True)
-biggestGainers.rename(columns={'Close':'Price'}, inplace=True)
-
-biggestLosers['% Loss'] = ['{0:.2f}'.format(x * 100) + '%' for x in biggestLosers['% Loss']]
-biggestGainers['% Gain'] = ['{0:.2f}'.format(x * 100) + '%' for x in biggestGainers['% Gain']]
+# fullGL = fullGL.merge(sAndPRef, how='left', on='Ticker')
 
 
-biggestLosers['Biggest Losses'] = biggestLosers[['Ticker', 'Company Name']].agg(', '.join, axis=1)
-biggestGainers['Biggest Gains'] = biggestGainers[['Ticker', 'Company Name']].agg(', '.join, axis=1)
+# biggestLosers = fullGL.sort_values(by=['1 Day Returns']).iloc[0:8,:].reset_index()
+# biggestGainers = fullGL.sort_values(by=['1 Day Returns'], ascending=False).iloc[0:8,:].reset_index()
 
-blFinal = biggestLosers[['Biggest Losses', 'Price\u061C', '% Loss']]
-bgFinal = biggestGainers[['Biggest Gains', 'Price', '% Gain']]
+# biggestLosers.rename(columns={'1 Day Returns':'% Loss'}, inplace=True)
+# biggestGainers.rename(columns={'1 Day Returns':'% Gain'}, inplace=True)
+
+# biggestLosers.rename(columns={'Close':'Price\u061C'}, inplace=True)
+# biggestGainers.rename(columns={'Close':'Price'}, inplace=True)
+
+# biggestLosers['% Loss'] = ['{0:.2f}'.format(x * 100) + '%' for x in biggestLosers['% Loss']]
+# biggestGainers['% Gain'] = ['{0:.2f}'.format(x * 100) + '%' for x in biggestGainers['% Gain']]
 
 
-biggestLG = pd.concat([bgFinal, blFinal], axis=1)
+# biggestLosers['Biggest Losses'] = biggestLosers[['Ticker', 'Company Name']].agg(', '.join, axis=1)
+# biggestGainers['Biggest Gains'] = biggestGainers[['Ticker', 'Company Name']].agg(', '.join, axis=1)
 
-biggestLG.to_csv('gl.csv', index=False)
+# blFinal = biggestLosers[['Biggest Losses', 'Price\u061C', '% Loss']]
+# bgFinal = biggestGainers[['Biggest Gains', 'Price', '% Gain']]
 
-fileDate = str(datetime.today().strftime('%B %d, %Y'))
 
-updateChart('k53KU', biggestLG, fileDate, ACCESS_TOKEN)
+# biggestLG = pd.concat([bgFinal, blFinal], axis=1)
+
+# biggestLG.to_csv('gl.csv', index=False)
+
+# fileDate = str(datetime.today().strftime('%B %d, %Y'))
+
+# updateChart('k53KU', biggestLG, fileDate, ACCESS_TOKEN)
